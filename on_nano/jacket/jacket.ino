c0dab78bf3583512c311df1884b451959c6b53b1
col-aatresh <anirudhashokaatresh@gmail.com>
2019-09-27 17:02:25 +0530

    modified code for nano

--- on_nano/jacket/jacket.ino
+++ on_nano/jacket/jacket.ino
@@ -1,57 +1,38 @@
-//#include<EEPROM.h>
-//#include<ESP8266WiFi.h>
-//#incldue<ESP8266WiFiMulti.h>
-
-#define from_jacket D6
-#define motor_led_flash D7
+#define from_jacket 6
+#define motor_led_flash 7
+#define from_trigger 8
+#define irleds 9
 
 void falling_edge();
 void rising_edge();
 void getting_shot();
+void fire();
+void shoot_ir(unsigned );
 
 // Player id: 1-> Team 1 ; 2-> Team 2;
 const char id=1,damage=10;
-volatile unsigned long start=0,stopp=0,delta=0,ml_start=0;
-unsigned int burst_len=0,health=100,god_burst_len=10,hit=0;
-const char* ssid="";
-const char* password="";
-
-//ESP8266WiFiMulti wifimulti;
+volatile unsigned long start=0,stopp=0,delta=0,ml_start=0,shoot=0;
+unsigned int jacket_burstLen=0,gun_burstLen=0,health=100,god_burst_len=10,hit=0;
 
 void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(115200);
-  
-  /*wifimulti.addAP(ssid,password);
-  
-  Serial.println("Connecting ...");
-  
-  while(wifimulti.run() != WL_CONNECTED)
-  {
-      delay(500);
-      Serial.print(".");
-  }
-  Serial.println();
-  
-  Serial.print("SSID:");
-  Serial.println(WiFi.SSID());
-  Serial.print("IP:");
-  Serial.println(WiFi.localIP());*/
-
 
   //health=EEPROM.read(0x00);
 
   attachInterrupt(digitalPinToInterrupt(from_jacket),falling_edge,FALLING);
-//  attachInterrupt(digitalPinToInterrupt(from_jacket),rising_edge,RISING);
+  attachInterrupt(digitalPinToInterrupt(from_trigger),fire,FALLING);
 
   switch(id)
   {
     case 1:
-      burst_len=3;
+      jacket_burstLen=5;
+      gun_burstLen=3;
       break;
     case 2:
-      burst_len=1000;    
+      jacket_burstLen=3;   
+      gun_burstLen=5;
       break;
   }
 
@@ -59,12 +40,20 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  if(start!=0)
+  
+  
+  if(shoot!=0)
+  {
+     detachInterrupt(digitalPinToInterrupt(from_trigger)); 
+     shoot=0; 
+     shoot_ir(gun_burstLen);
+     attachInterrupt(digitalPinToInterrupt(from_trigger),fire,FALLING);
+  }
+  else if(start!=0)
   {
     detachInterrupt(digitalPinToInterrupt(from_jacket));
 
-    while(digitalRead(D6)==0)
+    while(digitalRead(from_jacket)==0)
     {
       ;
     }
@@ -82,22 +71,21 @@ void loop() {
     start=0;
     stopp=0;
 
-
     
-    if(delta>god_burst_len && delta<god_burst_len)
+    if(delta>god_burst_len-1 && delta<god_burst_len+1)
     {
       //God gun firing
       health=100;
-
+      
+      Serial.println("God gun replinished my health");
+      
       //update eeprom
       //EEPROM.update(0x00,health);
 
       //update health bar
-
-      //update server
       
     }
-    else if((delta>burst_len-1 && delta<burst_len+1) && health!=0)
+    else if((delta>jacket_burstLen-1 && delta<jacket_burstLen+1) && health!=0)
     {      
       // Enemy fire ; Reduce health
 
@@ -111,18 +99,13 @@ void loop() {
       // update eeprom
       //EEPROM.update(0x00,health);
       
-      // update server
-      //******************************
-      Serial.println("Health updated on server");
-      //******************************
-      
       // vibrate and notify getting shot
       Serial.println("Motor vibrated");
       hit=1;
     }
 
     attachInterrupt(digitalPinToInterrupt(from_jacket),falling_edge,FALLING);
-    //attachInterrupt(digitalPinToInterrupt(from_jacket),rising_edge,RISING);
+
   }
 
   if(health==0)
@@ -145,22 +128,28 @@ void loop() {
     ml_start=0;
     digitalWrite(motor_led_flash,0);
   }
-  
-  
-//  attachInterrupt(digitalPinToInterrupt(from_jacket),falling_edge,FALLING);
-//  attachInterrupt(digitalPinToInterrupt(from_jacket),rising_edge,RISING);  
 
 }
 
 void falling_edge()
 {
+  // ********************************************** check if millis() works better ******************************************  
   start=micros();  
 }
 
-void rising_edge()
+void fire()
 {
-  stopp=micros();
+   shoot=1; 
 }
 
-
+void shoot_ir(unsigned gun_burstLen)
+{
+    for(int i=1;i<=((gun_burstLen*1000)/26);i++)
+    {
+       digitalWrite(irleds,1);
+       delayMicroseconds(13);
+       digitalWrite(irleds,0);
+       delayMicroseconds(13);  
+    }
+}
 
